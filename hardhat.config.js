5e9922aa784f49066906740a34ba58130dab3d76
Jaffar <jacepac264@gmail.com>
2022-07-06 16:45:28 +0300

    added advanced hardhat features

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,39 +6,43 @@ require("hardhat-gas-reporter")
 require("solidity-coverage")
 require("hardhat-deploy")
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners()
+const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL
+const KOVAN_PRIVATE_KEY = process.env.KOVAN_PRIVATE_KEY
 
-    for (const account of accounts) {
-        console.log(account.address)
-    }
-})
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
 module.exports = {
-    solidity: "0.8.7",
+    solidity: {
+        compilers: [{ version: "0.8.7" }, { version: "0.6.6" }],
+    },
     networks: {
-        ropsten: {
-            url: process.env.ROPSTEN_URL || "",
-            accounts:
-                process.env.PRIVATE_KEY !== undefined
-                    ? [process.env.PRIVATE_KEY]
-                    : [],
+        kovan: {
+            url: KOVAN_RPC_URL,
+            accounts: [KOVAN_PRIVATE_KEY],
+            chainId: 42,
+            blockConfirmations: 6
+        },
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: [RINKEBY_PRIVATE_KEY],
+            chainId: 4,
+            blockConfirmations: 6
         },
     },
     gasReporter: {
-        enabled: process.env.REPORT_GAS !== undefined,
+        enabled: true,
         currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
     },
     etherscan: {
-        apiKey: process.env.ETHERSCAN_API_KEY,
+        apiKey: ETHERSCAN_API_KEY,
     },
     namedAccounts: {
         deployer: {
