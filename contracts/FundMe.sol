9a11b866e41c1e867933f837f849e7b35d41f4e1
Jaffar <jacepac264@gmail.com>
2022-07-06 21:48:39 +0300

    updated contract to follow best practices

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -3,8 +3,13 @@ pragma solidity ^0.8.0;
 
 import "./PriceConverter.sol";
 
-error NotOwner();
+error FuneMe__NotOwner();
 
+/** @title A contract for crown funding
+ *  @author Jaffar Totanji
+ *  @notice This contract is a demo used for practice
+ *  @dev This implements price feeds as our library
+ */
 contract FundMe {
     using PriceConverter for uint256;
 
@@ -12,41 +17,55 @@ contract FundMe {
     address public immutable i_owner;
     address[] public funders;
     mapping(address => uint256) funderToAmount;
-
     AggregatorV3Interface public priceFeed;
 
-    constructor (address priceFeedAddress){
+    modifier onlyOwner() {
+        //require(msg.sender == i_owner, "Sender is not owner!!!");
+        if (msg.sender != i_owner) {
+            revert FuneMe__NotOwner();
+        }
+        _;
+    }
+
+    constructor(address priceFeedAddress) {
         i_owner = msg.sender;
         priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
-    function fund() public payable {
-        require(msg.value.getConversionRate(priceFeed) > MINIMUM_USD, "too low");
-        funders.push(msg.sender);
-        funderToAmount[msg.sender] = msg.value;
-    }
-
-    function withdraw() public onlyOwner {
-        for(uint i = 0; i < funders.length; i++) {
-            address funder = funders[i];
-            funderToAmount[funder] = 0;
-        }
-        funders = new address[](0);
-        (bool sent,) = payable(msg.sender).call{value: address(this).balance}("");
-        require(sent, "Failed to send Ether");
-    }
-
-    modifier onlyOwner {
-        //require(msg.sender == i_owner, "Sender is not owner!!!");
-        if(msg.sender != i_owner) { revert NotOwner(); }
-        _;
-    }
-
-    receive () external payable {
+    receive() external payable {
         fund();
     }
 
     fallback() external payable {
         fund();
+    }
+
+    /**
+    *  @notice This function funds the contracts
+    *  @dev This implements price feeds as our library
+    */
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate(priceFeed) > MINIMUM_USD,
+            "too low"
+        );
+        funders.push(msg.sender);
+        funderToAmount[msg.sender] = msg.value;
+    }
+
+    /**
+    *  @notice This function allows the owner to withdraw funds
+    *  @dev This implements price feeds as our library
+    */
+    function withdraw() public onlyOwner {
+        for (uint256 i = 0; i < funders.length; i++) {
+            address funder = funders[i];
+            funderToAmount[funder] = 0;
+        }
+        funders = new address[](0);
+        (bool sent, ) = payable(msg.sender).call{value: address(this).balance}(
+            ""
+        );
+        require(sent, "Failed to send Ether");
     }
 }
